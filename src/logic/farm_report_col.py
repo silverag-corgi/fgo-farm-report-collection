ad6c0feca540cafcd5c2dcdebe431e2d070c5857
Rem_ <black.white.red.are.the.best@gmail.com>
2022-02-05 17:35:22 +0900

    feat: 周回報告収集機能を追加

---
+++ src/logic/farm_report_col.py
@@ -0,0 +1,322 @@
+import os
+import re
+from datetime import date, datetime, timedelta
+from logging import Logger
+
+import pandas as pd
+import python_lib_for_me as mylib
+import requests
+from bs4 import BeautifulSoup
+from bs4.element import ResultSet
+from requests.models import Response
+from src.util import const_util
+
+FARM_REPORT_LIST_FILE_PATH: str = './dest/farm_report_list_{0}.csv'
+FARM_REPORT_SUMMARY_FILE_PATH: str = './dest/farm_report_summary_{0}.csv'
+FARM_REPORT_LIST_HEADER: list[str] = \
+    ['quest_kind', 'posting_date', 'user_id', 'quest_place', 'num_of_farms', 'material']
+FARM_REPORT_LIST_HEADER_RAW: list[str] = FARM_REPORT_LIST_HEADER[1:6]
+FARM_REPORT_SUMMARY_HEADER: list[str] = ['user_id', 'user_name', 'num_of_farms']
+ENCODING: str = 'cp932'
+FARM_REPORT_SITE_URL: str = 'https://fgojunks.max747.org/harvest/contents/date/{0}.html'
+USER_INFO_SITE_URL: str = 'https://twpro.jp/{0}'
+
+
+def do_logic(
+        col_year_month: str,
+        min_num_of_farms: str,
+        quest_kind: str,
+        should_output_user_name: bool
+    ) -> None:
+    
+    '''ロジック実行'''
+    
+    try:
+        # ロガー取得
+        lg: Logger = mylib.get_logger(__name__)
+        lg.info(f'周回報告収集を開始します。')
+        
+        # Pandasオプション設定
+        pd.set_option('display.unicode.east_asian_width', True)
+        
+        # 実行要否の判定
+        today: date = datetime.today().date()
+        col_first_date: date = datetime.strptime(col_year_month + '-01', '%Y-%m-%d').date()
+        first_date_of_this_month: date = mylib.get_first_date_of_this_month(today)
+        if col_first_date <= first_date_of_this_month:
+            should_execute = True
+        else:
+            should_execute = False
+            lg.info(f'収集年月が未来のため処理を終了します。(col_year_month:{col_year_month})')
+        
+        if should_execute == True:
+            # 周回報告一覧ファイルパス、周回報告概要ファイルパスの生成
+            farm_report_list_file_path: str = FARM_REPORT_LIST_FILE_PATH.format(col_year_month)
+            farm_report_summary_file_path: str = FARM_REPORT_SUMMARY_FILE_PATH.format(col_year_month)
+            
+            # 周回報告一覧生成開始日付の設定
+            list_gen_start_date = __generate_list_gen_start_date(
+                farm_report_list_file_path, col_first_date)
+            
+            # 周回報告一覧生成終了日付の設定
+            list_gen_end_date = __generate_list_gen_end_date(
+                list_gen_start_date, col_first_date, first_date_of_this_month, today)
+            
+            # 周回報告一覧生成要否の判定
+            if list_gen_start_date is None \
+                or list_gen_start_date > list_gen_end_date:
+                should_generate = False
+                lg.info(f'周回報告一覧は最新です。({col_year_month})')
+            elif list_gen_start_date is not None \
+                and list_gen_start_date <= list_gen_end_date:
+                should_generate = True
+                lg.info(f'周回報告一覧を生成します。({list_gen_start_date} ~ {list_gen_end_date})')
+            
+            # 周回報告一覧ファイルの生成
+            if should_generate == True:
+                __generate_farm_report_list(
+                        list_gen_start_date,
+                        list_gen_end_date,
+                        farm_report_list_file_path
+                    )
+            
+            # 周回報告概要ファイルの生成
+            __generate_farm_report_summary(
+                    farm_report_list_file_path,
+                    quest_kind,
+                    min_num_of_farms,
+                    farm_report_summary_file_path,
+                    should_output_user_name
+                )
+        
+        lg.info(f'周回報告収集を終了します。')
+    except Exception as e:
+        raise(e)
+    
+    return None
+
+
+def __generate_list_gen_start_date(
+        farm_report_list_file_path: str,
+        col_first_date: date
+    ) -> date:
+    
+    '''周回報告一覧生成開始日付生成'''
+    
+    try:
+        # ロガー取得
+        lg: Logger = mylib.get_logger(__name__)
+        
+        # 収集年月の周回報告一覧ファイルの存在有無チェック
+        has_farm_report_list: bool = os.path.isfile(farm_report_list_file_path)
+        
+        # 周回報告一覧生成開始日付の生成
+        list_gen_start_date: date = None
+        if has_farm_report_list == True:
+            farm_report_list_data_frame: pd.DataFrame = pd.read_csv(
+                    farm_report_list_file_path,
+                    header=None,
+                    names=FARM_REPORT_LIST_HEADER,
+                    index_col=None,
+                    parse_dates=[FARM_REPORT_LIST_HEADER[1]],
+                    encoding=ENCODING
+                )
+            
+            posting_date_of_last_line_timestamp: pd.Timestamp = \
+                farm_report_list_data_frame[FARM_REPORT_LIST_HEADER[1]].tail(1).item()
+            posting_date_of_last_line: date = posting_date_of_last_line_timestamp.date()
+            
+            if posting_date_of_last_line != \
+                mylib.get_last_date_of_this_month(posting_date_of_last_line):
+                list_gen_start_date: date = posting_date_of_last_line + timedelta(days=1)
+            else:
+                list_gen_start_date: date = None
+        else:
+            list_gen_start_date: date = col_first_date
+    except Exception as e:
+        raise(e)
+    
+    return list_gen_start_date
+
+
+def __generate_list_gen_end_date(
+        list_gen_start_date: date,
+        col_first_date: date,
+        first_date_of_this_month: date,
+        today: date
+    ) -> None:
+    
+    '''周回報告一覧生成終了日付生成'''
+    
+    try:
+        list_gen_end_date = None
+        if list_gen_start_date != None \
+            and col_first_date == first_date_of_this_month:
+            list_gen_end_date: date = today + timedelta(days=-1)
+        elif list_gen_start_date != None \
+            and col_first_date != first_date_of_this_month:
+            list_gen_end_date: date = mylib.get_last_date_of_this_month(list_gen_start_date)
+    except Exception as e:
+        raise(e)
+    
+    return list_gen_end_date
+
+
+def __generate_farm_report_list(
+        list_gen_start_date: date,
+        list_gen_end_date: date,
+        farm_report_list_file_path: str
+    ) -> None:
+    
+    '''周回報告一覧生成'''
+    
+    try:
+        # ロガー取得
+        lg: Logger = mylib.get_logger(__name__)
+        
+        # 周回報告一覧作業変数の初期化
+        farm_report_list_data_frame_work: pd.DataFrame = pd.DataFrame(columns=FARM_REPORT_LIST_HEADER)
+        
+        # 開始日付から終了日付までの繰り返し
+        list_gen_date: date = None
+        for list_gen_date in mylib.gen_date_range(list_gen_start_date, list_gen_end_date):
+            # 周回報告サイトURLの生成
+            farm_report_site_url: str = FARM_REPORT_SITE_URL.format(list_gen_date)
+            lg.info(farm_report_site_url)
+            
+            # クエスト種別ごとの周回報告数の取得
+            response_for_bs: Response = requests.get(farm_report_site_url)
+            bs: BeautifulSoup = BeautifulSoup(response_for_bs.content, 'html.parser')
+            num_of_farm_reports_list: ResultSet = bs.find_all(class_='subtitle')
+            num_of_farm_reports_of_event_quest: int = __get_num_of_farm_reports(num_of_farm_reports_list[0].get_text())
+            num_of_farm_reports_of_normal_quest: int = __get_num_of_farm_reports(num_of_farm_reports_list[1].get_text())
+            
+            # 周回報告一覧データフレームの取得
+            farm_report_list_data_frames: list[pd.DataFrame] = pd.read_html(
+                    farm_report_site_url,
+                    index_col=None,
+                    parse_dates=[0]
+                )
+            
+            # 周回報告一覧(イベクエ)の取得
+            if num_of_farm_reports_of_event_quest > 0:
+                farm_report_list_data_frames[0].set_axis(FARM_REPORT_LIST_HEADER_RAW, axis='columns', inplace=True)
+                farm_report_list_data_frames[0].insert(0, FARM_REPORT_LIST_HEADER[0], 'イベクエ')
+                farm_report_list_data_frame_work = pd.concat(
+                        [farm_report_list_data_frame_work, farm_report_list_data_frames[0]]
+                    )
+            
+            # 周回報告一覧(通常クエ)の取得
+            if num_of_farm_reports_of_event_quest == 0 \
+                and num_of_farm_reports_of_normal_quest > 0:
+                farm_report_list_data_frames[0].set_axis(FARM_REPORT_LIST_HEADER_RAW, axis='columns', inplace=True)
+                farm_report_list_data_frames[0].insert(0, FARM_REPORT_LIST_HEADER[0], '通常クエ')
+                farm_report_list_data_frame_work = pd.concat(
+                        [farm_report_list_data_frame_work, farm_report_list_data_frames[0]]
+                    )
+            elif num_of_farm_reports_of_event_quest > 0 \
+                and num_of_farm_reports_of_normal_quest > 0:
+                farm_report_list_data_frames[1].set_axis(FARM_REPORT_LIST_HEADER_RAW, axis='columns', inplace=True)
+                farm_report_list_data_frames[1].insert(0, FARM_REPORT_LIST_HEADER[0], '通常クエ')
+                farm_report_list_data_frame_work = pd.concat(
+                        [farm_report_list_data_frame_work, farm_report_list_data_frames[1]]
+                    )
+        
+        # 周回報告一覧ファイルへの保存
+        farm_report_list_data_frame_work.reset_index(drop=True, inplace=True)
+        lg.info(f'周回報告一覧(追加分)\n{farm_report_list_data_frame_work.head(5)}')
+        lg.info(f'周回報告一覧(追加分)\n{farm_report_list_data_frame_work.tail(5)}')
+        farm_report_list_data_frame_work.to_csv(
+                farm_report_list_file_path,
+                header=False,
+                index=False,
+                mode='a',
+                encoding=ENCODING
+            )
+    except Exception as e:
+        raise(e)
+    
+    return None
+
+
+def __get_num_of_farm_reports(sub_title: str) -> int:
+    '''周回報告数取得'''
+    
+    try:
+        matched_result_of_event_quest = re.match('イベント \((.*)\)', sub_title)
+        matched_result_of_normal_quest = re.match('恒常フリークエスト \((.*)\)', sub_title)
+        
+        if matched_result_of_event_quest is not None:
+            num_of_farm_reports: int = int(matched_result_of_event_quest.group(1))
+        elif matched_result_of_normal_quest is not None:
+            num_of_farm_reports: int = int(matched_result_of_normal_quest.group(1))
+        else:
+            num_of_farm_reports: int = 0
+    except Exception as e:
+        raise(e)
+    
+    return num_of_farm_reports
+
+
+def __generate_farm_report_summary(
+        farm_report_list_file_path: str,
+        quest_kind: str,
+        min_num_of_farms: str,
+        farm_report_summary_file_path: str,
+        should_output_user_name: bool
+    ) -> None:
+    
+    '''周回報告概要ファイル生成'''
+    
+    try:
+        # ロガー取得
+        lg: Logger = mylib.get_logger(__name__)
+        
+        # 周回報告一覧ファイルの読み込み
+        farm_report_list_data_frame: pd.DataFrame = pd.read_csv(
+                farm_report_list_file_path,
+                header=None,
+                names=FARM_REPORT_LIST_HEADER,
+                index_col=None,
+                parse_dates=[FARM_REPORT_LIST_HEADER[1]],
+                encoding=ENCODING
+            )
+        
+        # クエスト種別による抽出
+        farm_report_summary_data_frame: pd.DataFrame = None
+        if quest_kind in const_util.QUEST_KINDS:
+            farm_report_summary_data_frame = farm_report_list_data_frame.query(
+                f'{FARM_REPORT_LIST_HEADER[0]}.str.match("{quest_kind}")')
+        
+        # 投稿者による集計
+        farm_report_summary_data_frame = farm_report_summary_data_frame.groupby(
+            FARM_REPORT_LIST_HEADER[2]).sum()
+        
+        # 周回数による降順ソート
+        farm_report_summary_data_frame.sort_values(
+            FARM_REPORT_LIST_HEADER[4], ascending=False, inplace=True)
+        
+        # 周回数による抽出
+        farm_report_summary_data_frame.query(
+            f'{FARM_REPORT_LIST_HEADER[4]} >= {min_num_of_farms}', inplace=True)
+        
+        # 列(ユーザ名)の追加
+        farm_report_summary_data_frame.insert(0, FARM_REPORT_SUMMARY_HEADER[1], '-')
+        
+        # TODO ユーザ名の設定
+        if should_output_user_name == True:
+            pass
+        
+        # 周回報告概要ファイルの保存
+        lg.info(f'\n{farm_report_summary_data_frame}')
+        farm_report_summary_data_frame.to_csv(
+                farm_report_summary_file_path,
+                header=False,
+                index=True,
+                mode='w',
+                encoding=ENCODING
+            )
+    except Exception as e:
+        raise(e)
+    
+    return None
---
+++ src/main/farm_report_col.py
@@ -0,0 +1,124 @@
+import argparse
+import os
+import sys
+from datetime import datetime
+from logging import Logger
+
+import python_lib_for_me
+from src.logic import farm_report_col
+from src.util import const_util
+
+
+def main():
+    '''メイン'''
+    
+    try:
+        # ロガー取得
+        lg: Logger = python_lib_for_me.get_logger(__name__)
+        
+        # 実行コマンド表示
+        sys.argv[0] = os.path.basename(sys.argv[0])
+        lg.info(f'実行コマンド：{sys.argv}')
+        
+        # 引数取得＆検証
+        args: argparse.Namespace = __get_args()
+        if __validate_args(args) == False:
+            return 1
+        
+        # 周回報告収集
+        if args.min_num_of_all_quest is not None:
+            farm_report_col.do_logic(
+                    args.col_year_month,
+                    args.min_num_of_all_quest,
+                    const_util.QUEST_KINDS[0],
+                    args.should_output_user_name
+                )
+        elif args.min_num_of_normal_quest is not None:
+            farm_report_col.do_logic(
+                    args.col_year_month,
+                    args.min_num_of_normal_quest,
+                    const_util.QUEST_KINDS[1],
+                    args.should_output_user_name
+                )
+        elif args.min_num_of_event_quest is not None:
+            farm_report_col.do_logic(
+                    args.col_year_month,
+                    args.min_num_of_event_quest,
+                    const_util.QUEST_KINDS[2],
+                    args.should_output_user_name
+                )
+    except Exception as e:
+        lg.exception('', exc_info=True)
+        return 1
+    
+    return 0
+
+
+def __get_args() -> argparse.Namespace:
+    '''引数取得'''
+    
+    try:
+        parser: argparse.ArgumentParser = argparse.ArgumentParser(
+                formatter_class=argparse.RawTextHelpFormatter,
+                exit_on_error=True
+            )
+        
+        help_msg: str = ''
+        
+        # 必須の引数
+        help_msg = '収集年月(yyyy-mm形式)'
+        parser.add_argument('col_year_month', help=help_msg)
+        
+        # グループで1つのみ必須の引数
+        group: argparse._MutuallyExclusiveGroup = parser.add_mutually_exclusive_group(required=True)
+        help_msg = '最低周回数({0})\nグループで1つのみ必須'
+        group.add_argument('-a', '--min_num_of_all_quest', type=int, help=help_msg.format('全て'))
+        group.add_argument('-n', '--min_num_of_normal_quest', type=int, help=help_msg.format('通常クエ'))
+        group.add_argument('-e', '--min_num_of_event_quest', type=int, help=help_msg.format('イベクエ'))
+        
+        # 任意の引数
+        help_msg = 'ユーザ名出力要否'
+        parser.add_argument('-u', '--should_output_user_name', help=help_msg, action='store_true')
+        
+        args: argparse.Namespace = parser.parse_args()
+    except Exception as e:
+        raise(e)
+    
+    return args
+
+
+def __validate_args(args: argparse.Namespace) -> bool:
+    '''引数検証'''
+    
+    try:
+        # ロガー取得
+        lg: Logger = python_lib_for_me.get_logger(__name__)
+        
+        # 検証：収集年月が年月(yyyy-mm形式)であること
+        try:
+            datetime.strptime(args.col_year_month, '%Y-%m')
+        except ValueError:
+            lg.info(f'収集年月が年月(yyyy-mm形式)ではありません。(col_year_month:{args.col_year_month})')
+            return False
+        
+        # 検証：最低周回数のいずれかが0以上であること
+        if args.min_num_of_all_quest is not None \
+            and args.min_num_of_all_quest < 0:
+            lg.info(f'最低周回数(全て)が0以上ではありません。(min_num_of_all_quest:{args.min_num_of_all_quest})')
+            return False
+        elif args.min_num_of_normal_quest is not None \
+            and args.min_num_of_normal_quest < 0:
+            lg.info(f'最低周回数(通常クエ)が0以上ではありません。(min_num_of_normal_quest:{args.min_num_of_normal_quest})')
+            return False
+        elif args.min_num_of_event_quest is not None \
+            and args.min_num_of_event_quest < 0:
+            lg.info(f'最低周回数(イベクエ)が0以上ではありません。(min_num_of_event_quest:{args.min_num_of_event_quest})')
+            return False
+    except Exception as e:
+        raise(e)
+    
+    return True
+
+
+if __name__ == '__main__':
+    sys.exit(main())
