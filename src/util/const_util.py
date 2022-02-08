2968b678e6c600e6b7ad51c9c3826409cd8f5fd2
Rem_ <black.white.red.are.the.best@gmail.com>
2022-02-09 08:28:29 +0900

    feat: ファイル生成時の文字コードをutf8に変更
    元々はcsvをexcelでも開く予定だったため、
    ファイル生成時の文字コードをshiftjisで指定していた。
    しかし、現在はsakuraやeditcsvでしか開かない上に、
    ユーザ名に環境依存文字がある場合にcsv生成でエラーが発生するため、
    utf8に変更した。

--- src/util/const_util.py
+++ src/util/const_util.py
@@ -6,7 +6,7 @@ FARM_REPORT_LIST_HEADER: Final[list[str]] = \
     ['quest_kind', 'posting_date', 'user_id', 'quest_place', 'num_of_farms', 'material']
 FARM_REPORT_LIST_HEADER_RAW: Final[list[str]] = FARM_REPORT_LIST_HEADER[1:6]
 FARM_REPORT_SUMMARY_HEADER: Final[list[str]] = ['user_id', 'user_name', 'num_of_farms']
-ENCODING: Final[str] = 'cp932'
+ENCODING: Final[str] = 'utf8'
 FARM_REPORT_SITE_URL: Final[str] = 'https://fgojunks.max747.org/harvest/contents/date/{0}.html'
 USER_INFO_SITE_URL: Final[str] = 'https://twpro.jp/{0}'
 QUEST_KINDS: Final[list[str]] = ['全て', '通常クエ', 'イベクエ']
