7dab50ede0ce18b76fbee176317a629a98e5df36
Rem_ <black.white.red.are.the.best@gmail.com>
2022-02-08 23:00:05 +0900

    refactor: コードを改善
    モジュールを目的ごとに分割
    周回報告関連ファイルのパスを変更

---
+++ src/logic/farm_report_summary_gen.py
@@ -0,0 +1,129 @@
+from datetime import date, datetime
+from logging import Logger
+from typing import Optional
+
+import pandas as pd
+import python_lib_for_me as mylib
+from src.util import *
+
+
+def do_logic(
+        col_year_month: str,
+        min_num_of_farms: int,
+        quest_kind: str,
+        should_output_user_name: bool
+    ) -> None:
+    
+    '''ロジック実行'''
+    
+    lg: Optional[Logger] = None
+    
+    try:
+        # ロガー取得
+        lg = mylib.get_logger(__name__)
+        lg.info(f'周回報告概要生成を開始します。')
+        
+        # Pandasオプション設定
+        pd.set_option('display.unicode.east_asian_width', True)
+        
+        # 実行要否の判定
+        today: date = datetime.today().date()
+        col_first_date: date = datetime.strptime(col_year_month + '-01', '%Y-%m-%d').date()
+        first_date_of_this_month: date = mylib.get_first_date_of_this_month(today)
+        if col_first_date <= first_date_of_this_month:
+            should_execute = True
+        else:
+            should_execute = False
+            lg.info(f'収集年月が未来です。(col_year_month:{col_year_month})')
+        
+        if should_execute == True:
+            # 周回報告一覧ファイルパス、周回報告概要ファイルパスの生成
+            farm_report_list_file_path: str = \
+                const_util.FARM_REPORT_LIST_FILE_PATH.format(col_year_month)
+            farm_report_summary_file_path: str = \
+                const_util.FARM_REPORT_SUMMARY_FILE_PATH.format(col_year_month, quest_kind, f'{min_num_of_farms}周以上')
+            
+            # 周回報告概要ファイルの生成
+            lg.info(f'周回報告概要ファイル：{farm_report_summary_file_path}')
+            __generate_farm_report_summary(
+                    farm_report_list_file_path,
+                    quest_kind,
+                    min_num_of_farms,
+                    farm_report_summary_file_path,
+                    should_output_user_name
+                )
+        
+        lg.info(f'周回報告概要生成を終了します。')
+    except Exception as e:
+        raise(e)
+    
+    return None
+
+
+def __generate_farm_report_summary(
+        farm_report_list_file_path: str,
+        quest_kind: str,
+        min_num_of_farms: int,
+        farm_report_summary_file_path: str,
+        should_output_user_name: bool
+    ) -> None:
+    
+    '''周回報告概要ファイル生成'''
+    
+    lg: Optional[Logger] = None
+    
+    try:
+        # ロガー取得
+        lg = mylib.get_logger(__name__)
+        
+        # 周回報告一覧ファイルの読み込み
+        farm_report_list_data_frame: pd.DataFrame = pd.read_csv(
+                farm_report_list_file_path,
+                header=None,
+                names=const_util.FARM_REPORT_LIST_HEADER,
+                index_col=None,
+                parse_dates=[const_util.FARM_REPORT_LIST_HEADER[1]],
+                encoding=const_util.ENCODING
+            )
+        
+        if quest_kind in const_util.QUEST_KINDS:
+            # クエスト種別による抽出
+            if quest_kind in const_util.QUEST_KINDS[1:3]:
+                farm_report_summary_data_frame: pd.DataFrame = farm_report_list_data_frame.query(
+                    f'{const_util.FARM_REPORT_LIST_HEADER[0]}.str.match("{quest_kind}")')
+            else:
+                farm_report_summary_data_frame: pd.DataFrame = farm_report_list_data_frame
+        
+            # 投稿者による集計
+            farm_report_summary_data_frame = farm_report_summary_data_frame.groupby(
+                const_util.FARM_REPORT_LIST_HEADER[2]).sum()
+            
+            # 周回数による降順ソート
+            farm_report_summary_data_frame.sort_values(
+                const_util.FARM_REPORT_LIST_HEADER[4], ascending=False, inplace=True)
+            
+            # 周回数による抽出
+            farm_report_summary_data_frame.query(
+                f'{const_util.FARM_REPORT_LIST_HEADER[4]} >= {min_num_of_farms}', inplace=True)
+            
+            # 列(ユーザ名)の追加
+            farm_report_summary_data_frame.insert(
+                0, const_util.FARM_REPORT_SUMMARY_HEADER[1], '-')
+            
+            # TODO ユーザ名の設定
+            if should_output_user_name == True:
+                pass
+            
+            # 周回報告概要ファイルの保存
+            lg.info(f'\n{farm_report_summary_data_frame}')
+            farm_report_summary_data_frame.to_csv(
+                    farm_report_summary_file_path,
+                    header=False,
+                    index=True,
+                    mode='w',
+                    encoding=const_util.ENCODING
+                )
+    except Exception as e:
+        raise(e)
+    
+    return None
---
+++ src/main/farm_report_list_gen.py
@@ -0,0 +1,107 @@
+import argparse
+import os
+import sys
+from datetime import datetime
+from logging import Logger
+from typing import Optional
+
+import python_lib_for_me
+from src.logic import *
+from src.util import *
+
+
+def main() -> int:
+    
+    '''
+    メイン
+    
+    Summary:
+        コマンドラインから実行する。
+        
+        引数を検証して問題ない場合、周回報告一覧ファイルを生成する。
+    
+    Args:
+        -
+    
+    Args on cmd line:
+        col_year_month (str): [必須] 収集年月(yyyy-mm形式)
+    
+    Returns:
+        int: 終了コード(0：正常、1：異常)
+    
+    Destinations:
+        周回報告一覧ファイル: ./dest/farm_report_list/farm_report_list_[収集年月].csv
+    '''
+    
+    lg: Optional[Logger] = None
+    
+    try:
+        # ロガー取得
+        lg = python_lib_for_me.get_logger(__name__)
+        
+        # 実行コマンド表示
+        sys.argv[0] = os.path.basename(sys.argv[0])
+        lg.info(f'実行コマンド：{sys.argv}')
+        
+        # 引数取得＆検証
+        args: argparse.Namespace = __get_args()
+        if __validate_args(args) == False:
+            return 1
+        
+        # 周回報告一覧ファイル生成
+        farm_report_list_gen.do_logic(
+                args.col_year_month
+            )
+    except Exception as e:
+        if lg is not None:
+            lg.exception('', exc_info=True)
+        return 1
+    
+    return 0
+
+
+def __get_args() -> argparse.Namespace:
+    '''引数取得'''
+    
+    try:
+        parser: argparse.ArgumentParser = argparse.ArgumentParser(
+                formatter_class=argparse.RawTextHelpFormatter,
+                exit_on_error=True
+            )
+        
+        help_msg: str = ''
+        
+        # 必須の引数
+        help_msg = '収集年月(yyyy-mm形式)'
+        parser.add_argument('col_year_month', help=help_msg)
+        
+        args: argparse.Namespace = parser.parse_args()
+    except Exception as e:
+        raise(e)
+    
+    return args
+
+
+def __validate_args(args: argparse.Namespace) -> bool:
+    '''引数検証'''
+    
+    lg: Optional[Logger] = None
+    
+    try:
+        # ロガー取得
+        lg = python_lib_for_me.get_logger(__name__)
+        
+        # 検証：収集年月が年月(yyyy-mm形式)であること
+        try:
+            datetime.strptime(args.col_year_month, '%Y-%m')
+        except ValueError:
+            lg.info(f'収集年月が年月(yyyy-mm形式)ではありません。(col_year_month:{args.col_year_month})')
+            return False
+    except Exception as e:
+        raise(e)
+    
+    return True
+
+
+if __name__ == '__main__':
+    sys.exit(main())
