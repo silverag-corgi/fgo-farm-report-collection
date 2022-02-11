605a5b91627d842a71bc5ad131f1aac11333730e
Rem_ <black.white.red.are.the.best@gmail.com>
2022-02-11 20:18:05 +0900

    [c_add] 周回報告実績生成を実装

---
+++ src/logic/farm_report_performance_gen.py
@@ -0,0 +1,141 @@
+import os
+from logging import Logger
+from typing import Optional
+
+import pandas as pd
+import python_lib_for_me as mylib
+from src.logic import farm_report_list_gen
+from src.util import *
+
+
+def do_logic(
+        col_year: str,
+        user_id: str,
+        should_generate_list: bool
+    ) -> None:
+    
+    '''ロジック実行'''
+    
+    lg: Optional[Logger] = None
+    
+    try:
+        # ロガー取得
+        lg = mylib.get_logger(__name__)
+        lg.info(f'周回報告実績生成を開始します。')
+        
+        # Pandasオプション設定
+        pd.set_option('display.unicode.east_asian_width', True)
+        
+        # データフレーム用の投稿年月の生成
+        posting_month: list[str] = []
+        for index in range(NUM_OF_LINES_IN_FARM_REPORT_PERFORMANCE):
+            posting_month.append(f'{col_year:02}-{(index+1):02}')
+        
+        # 周回報告実績データフレームの初期化
+        farm_report_performance_df: pd.DataFrame = pd.DataFrame(
+                index=range(NUM_OF_LINES_IN_FARM_REPORT_PERFORMANCE),
+                columns=const_util.FARM_REPORT_PERFORMANCE_HEADER
+            )
+        farm_report_performance_df[
+            const_util.FARM_REPORT_PERFORMANCE_HEADER[0]] = posting_month
+        farm_report_performance_df.fillna(0, inplace=True)
+        
+        # 指定したユーザの周回数の集計
+        for index in range(NUM_OF_LINES_IN_FARM_REPORT_PERFORMANCE):
+            # 収集年月の生成
+            col_year_month: str = f'{col_year:02}-{(index+1):02}'
+            
+            # 周回報告一覧ファイルパスの生成
+            farm_report_list_file_path: str = \
+                const_util.FARM_REPORT_LIST_FILE_PATH.format(col_year_month)
+            
+            # 周回報告実績更新要否の判定
+            should_update: bool = True
+            if os.path.isfile(farm_report_list_file_path) == False:
+                if should_generate_list == True:
+                    # 周回報告一覧生成ロジックの実行
+                    farm_report_list_gen.do_logic(col_year_month)
+                    
+                    # 周回報告一覧ファイルの存在有無チェック
+                    if os.path.isfile(farm_report_list_file_path) == False:
+                        should_update = False
+                else:
+                    should_update = False
+            
+            # 周回報告実績データフレームの更新
+            if should_update == True:
+                __update_farm_report_performance_data_frame(
+                        farm_report_list_file_path,
+                        user_id,
+                        farm_report_performance_df,
+                        index
+                    )
+        
+        # 周回報告実績ファイルパスの生成
+        farm_report_performance_file_path: str = \
+            const_util.FARM_REPORT_PERFORMANCE_FILE_PATH.format(col_year, user_id)
+        
+        # 周回報告実績ファイルへの保存
+        lg.info(f'周回報告実績\n{farm_report_performance_df}')
+        farm_report_performance_df.to_csv(
+                farm_report_performance_file_path,
+                header=True,
+                index=False,
+                mode='w',
+                encoding=const_util.ENCODING
+            )
+        
+        lg.info(f'周回報告実績生成を終了します。')
+    except Exception as e:
+        raise(e)
+    
+    return None
+
+
+def __update_farm_report_performance_data_frame(
+        farm_report_list_file_path: str,
+        user_id: str,
+        farm_report_performance_df: pd.DataFrame,
+        index: int
+    ) -> None:
+    
+    '''周回報告実績データフレーム更新'''
+    
+    lg: Optional[Logger] = None
+    
+    try:
+        # ロガー取得
+        lg = mylib.get_logger(__name__)
+        
+        # 周回報告一覧ファイルの読み込み
+        farm_report_list_df: pd.DataFrame = pd.read_csv(
+                farm_report_list_file_path,
+                header=None,
+                names=const_util.FARM_REPORT_LIST_HEADER,
+                index_col=None,
+                parse_dates=[const_util.FARM_REPORT_LIST_HEADER[1]],
+                encoding=const_util.ENCODING
+            )
+        
+        # ユーザID、クエスト種別による抽出
+        df_by_user_id: pd.DataFrame = \
+            farm_report_list_df.query(
+                f'{const_util.FARM_REPORT_LIST_HEADER[2]}.str.match("^{user_id}$")')
+        df_by_user_id_and_normal_quest: pd.DataFrame = \
+            df_by_user_id.query(
+                f'{const_util.FARM_REPORT_LIST_HEADER[0]}.str.match("{const_util.QUEST_KINDS[1]}")')
+        df_by_user_id_and_event_quest: pd.DataFrame = \
+            df_by_user_id.query(
+                f'{const_util.FARM_REPORT_LIST_HEADER[0]}.str.match("{const_util.QUEST_KINDS[2]}")')
+        
+        # 周回数の更新
+        farm_report_performance_df.at[index, const_util.FARM_REPORT_PERFORMANCE_HEADER[1]] = \
+            df_by_user_id_and_normal_quest[const_util.FARM_REPORT_LIST_HEADER[4]].sum()
+        farm_report_performance_df.at[index, const_util.FARM_REPORT_PERFORMANCE_HEADER[2]] = \
+            df_by_user_id_and_event_quest[const_util.FARM_REPORT_LIST_HEADER[4]].sum()
+        farm_report_performance_df.at[index, const_util.FARM_REPORT_PERFORMANCE_HEADER[3]] = \
+            df_by_user_id[const_util.FARM_REPORT_LIST_HEADER[4]].sum()
+    except Exception as e:
+        raise(e)
+    
+    return None
---
+++ src/main/farm_report_performance_gen.py
@@ -0,0 +1,119 @@
+import argparse
+import os
+import sys
+from datetime import datetime
+from logging import Logger
+from typing import Optional
+
+import python_lib_for_me
+from src.logic import farm_report_performance_gen
+from src.util import *
+
+
+def main() -> int:
+    
+    '''
+    メイン
+    
+    Summary:
+        コマンドラインから実行する。
+        
+        引数を検証して問題ない場合、周回報告一覧ファイルと周回報告実績ファイルを生成する。
+    
+    Args:
+        -
+    
+    Args on cmd line:
+        col_year (str)               : [必須] 収集年(yyyy形式)
+        user_id (str)                : [必須] ユーザID(Twitter)
+        should_generate_list (bool)  : [任意] 周回報告一覧生成要否
+    
+    Returns:
+        int: 終了コード(0：正常、1：異常)
+    
+    Destinations:
+        周回報告一覧ファイル: ./dest/farm_report_list/farm_report_list_[収集年月].csv
+        周回報告実績ファイル: ./dest/farm_report_performance/farm_report_performance_[収集年]_[ユーザID].csv
+    '''
+    
+    lg: Optional[Logger] = None
+    
+    try:
+        # ロガー取得
+        lg = python_lib_for_me.get_logger(__name__)
+        
+        # 実行コマンド表示
+        sys.argv[0] = os.path.basename(sys.argv[0])
+        lg.info(f'実行コマンド：{sys.argv}')
+        
+        # 引数取得＆検証
+        args: argparse.Namespace = __get_args()
+        if __validate_args(args) == False:
+            return 1
+        
+        # 周回報告実績ファイル生成
+        farm_report_performance_gen.do_logic(
+                args.col_year,
+                args.user_id,
+                bool(args.should_generate_list)
+            )
+    except Exception as e:
+        if lg is not None:
+            lg.exception('', exc_info=True)
+        return 1
+    
+    return 0
+
+
+def __get_args() -> argparse.Namespace:
+    '''引数取得'''
+    
+    try:
+        parser: argparse.ArgumentParser = argparse.ArgumentParser(
+                formatter_class=argparse.RawTextHelpFormatter,
+                exit_on_error=True
+            )
+        
+        help_msg: str = ''
+        
+        # 必須の引数
+        help_msg = '収集年(yyyy形式)'
+        parser.add_argument('col_year', help=help_msg)
+        help_msg = 'ユーザID(Twitter)'
+        parser.add_argument('user_id', help=help_msg)
+        
+        # 任意の引数
+        help_msg = '周回報告一覧生成要否\n' + \
+            '指定した場合は一覧を生成するが、指定しなかった場合は生成せずに既存の一覧のみを使用する。'
+        parser.add_argument('-l', '--should_generate_list', help=help_msg, action='store_true')
+        
+        args: argparse.Namespace = parser.parse_args()
+    except Exception as e:
+        raise(e)
+    
+    return args
+
+
+def __validate_args(args: argparse.Namespace) -> bool:
+    '''引数検証'''
+    
+    lg: Optional[Logger] = None
+    
+    try:
+        # ロガー取得
+        lg = python_lib_for_me.get_logger(__name__)
+        
+        # 検証：収集年が年(yyyy形式)であること
+        try:
+            datetime.strptime(args.col_year, '%Y')
+        except ValueError:
+            lg.info(f'収集年が年(yyyy形式)ではありません。(col_year:{args.col_year})')
+            return False
+    except Exception as e:
+        raise(e)
+    
+    return True
+
+
+if __name__ == '__main__':
+    sys.exit(main())
