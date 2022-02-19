6f01280521a4dc6584d3c69702a082783f90c876
Rem_ <black.white.red.are.the.best@gmail.com>
2022-02-19 17:14:00 +0900

    refactor: コードを改善
    docstring、ヘルプ、ロジックを整理

--- src/main/farm_report_total_summary_gen.py
+++ src/main/farm_report_total_summary_gen.py
@@ -18,7 +18,9 @@ def main() -> int:
     Summary:
         コマンドラインから実行する。
         
-        引数を検証して問題ない場合、周回報告一覧ファイルと周回報告全体概要ファイルを生成する。
+        引数を検証して問題ない場合、周回報告一覧ファイルを基に周回報告全体概要ファイルを生成する。
+        
+        任意で周回報告一覧ファイルを生成する。
     
     Args:
         -
@@ -29,7 +31,7 @@ def main() -> int:
         min_num_of_normal_quest (int)   : [いずれかが必須] 最低周回数(通常クエ)
         min_num_of_event_quest (int)    : [いずれかが必須] 最低周回数(イベクエ)
         should_generate_list (bool)     : [任意] 周回報告一覧生成要否
-        should_output_user_name (bool)  : [任意] ユーザ名出力要否(概要ファイルにユーザ名を出力するか)
+        should_output_user_name (bool)  : [任意] ユーザ名出力要否
     
     Returns:
         int: 終了コード(0：正常、1：異常)
@@ -117,10 +119,12 @@ def __get_args() -> argparse.Namespace:
             '-e', '--min_num_of_event_quest', type=int, help=help_msg.format('イベクエ'))
         
         # 任意の引数
-        help_msg = '周回報告一覧生成要否\n' + \
-            '指定した場合は一覧を生成するが、指定しなかった場合は生成せずに既存の一覧のみを使用する。'
+        help_msg =  '周回報告一覧生成要否\n' + \
+                    '指定した場合は一覧を生成する。\n' + \
+                    '指定しなかった場合は生成せずに既存の一覧のみを使用する。'
         parser.add_argument('-l', '--should_generate_list', help=help_msg, action='store_true')
-        help_msg = 'ユーザ名出力要否'
+        help_msg =  'ユーザ名出力要否\n' + \
+                    '指定した場合は周回報告概要ファイルにユーザ名を出力する。'
         parser.add_argument('-u', '--should_output_user_name', help=help_msg, action='store_true')
         
         args: argparse.Namespace = parser.parse_args()
@@ -143,21 +147,25 @@ def __validate_args(args: argparse.Namespace) -> bool:
         try:
             datetime.strptime(args.col_year_month, '%Y-%m')
         except ValueError:
-            lg.info(f'収集年月が年月(yyyy-mm形式)ではありません。(col_year_month:{args.col_year_month})')
+            lg.info(f'収集年月が年月(yyyy-mm形式)ではありません。' +
+                    f'(col_year_month:{args.col_year_month})')
             return False
         
         # 検証：最低周回数のいずれかが0以上であること
         if args.min_num_of_all_quest is not None \
-            and args.min_num_of_all_quest < 0:
-            lg.info(f'最低周回数(全て)が0以上ではありません。(min_num_of_all_quest:{args.min_num_of_all_quest})')
+            and not (args.min_num_of_all_quest >= 0):
+            lg.info(f'最低周回数(全て)が0以上ではありません。' +
+                    f'(min_num_of_all_quest:{args.min_num_of_all_quest})')
             return False
         elif args.min_num_of_normal_quest is not None \
-            and args.min_num_of_normal_quest < 0:
-            lg.info(f'最低周回数(通常クエ)が0以上ではありません。(min_num_of_normal_quest:{args.min_num_of_normal_quest})')
+            and not (args.min_num_of_normal_quest >= 0):
+            lg.info(f'最低周回数(通常クエ)が0以上ではありません。' +
+                    f'(min_num_of_normal_quest:{args.min_num_of_normal_quest})')
             return False
         elif args.min_num_of_event_quest is not None \
-            and args.min_num_of_event_quest < 0:
-            lg.info(f'最低周回数(イベクエ)が0以上ではありません。(min_num_of_event_quest:{args.min_num_of_event_quest})')
+            and not (args.min_num_of_event_quest >= 0):
+            lg.info(f'最低周回数(イベクエ)が0以上ではありません。' +
+                    f'(min_num_of_event_quest:{args.min_num_of_event_quest})')
             return False
     except Exception as e:
         raise(e)
