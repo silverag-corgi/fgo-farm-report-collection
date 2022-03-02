9f56718eec310d62538042e8a9e038025befa4a5
Rem_ <black.white.red.are.the.best@gmail.com>
2022-03-03 04:41:04 +0900

    fix: 生成済み一覧ファイルから更新できない件を修正
    周回報告一覧ファイルが存在する・しないに関わらず、
    一覧生成ロジックを実行する処理に変更
    周回報告一覧ファイルが存在する場合に更新する処理を考慮していなかったため

--- src/fgo_farm_report_collection/logic/farm_report_individual_summary_gen.py
+++ src/fgo_farm_report_collection/logic/farm_report_individual_summary_gen.py
@@ -45,17 +45,16 @@ def do_logic(
             
             # 周回報告個人概要更新の判定
             update_ind_sum: bool = True
-            if os.path.isfile(farm_report_list_file_path) == False:
-                if generate_list == True:
-                    # 周回報告一覧生成ロジックの実行
-                    pyl.measure_proc_time(
-                        farm_report_list_gen.do_logic_by_col_year_month)(col_year_month)
-                    
-                    # 周回報告一覧ファイルの存在有無チェック
-                    if os.path.isfile(farm_report_list_file_path) == False:
-                        update_ind_sum = False
-                else:
+            if generate_list == True:
+                # 周回報告一覧生成ロジックの実行
+                pyl.measure_proc_time(
+                    farm_report_list_gen.do_logic_by_col_year_month)(col_year_month)
+                
+                # 周回報告一覧ファイルの存在有無チェック
+                if os.path.isfile(farm_report_list_file_path) == False:
                     update_ind_sum = False
+            else:
+                update_ind_sum = False
             
             # 周回報告個人概要データフレームの更新
             farm_report_ind_sum_df.at[
